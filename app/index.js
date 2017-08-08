2c30132562afb4f03c1fd77d02c40b2d3d7420cd
Chris Bobbe <csbobbe@gmail.com>
2017-08-07 23:25:11 -0400

    Add react-tap-event-plugin for mobile responsiveness

--- app/index.js
+++ app/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+import App from './components/App.jsx';
 
 ReactDOM.render(
   (<MuiThemeProvider>
